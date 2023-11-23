fbe9c01de0b5eee4542c5160df0fce70f6d7460e
JUNYI OU <49964599+Irvingouj@users.noreply.github.com>
2023-11-22 22:56:47 -0500

    fix path error on windows

--- auto_subtitle/cli.py
+++ auto_subtitle/cli.py
@@ -1,9 +1,12 @@
 import os
+import subprocess
+from typing import Dict, List
 import ffmpeg
 import whisper
 import argparse
 import warnings
 import tempfile
+from pathlib import Path
 from .utils import filename, str2bool, write_srt
 
 
@@ -34,6 +37,9 @@ def main():
     output_srt: bool = args.pop("output_srt")
     srt_only: bool = args.pop("srt_only")
     language: str = args.pop("language")
+
+    video_pathes = args.pop("video")
+    good_video_pathes = [Path(p).resolve() for p in video_pathes]
     
     os.makedirs(output_dir, exist_ok=True)
 
@@ -46,58 +52,67 @@ def main():
         args["language"] = language
         
     model = whisper.load_model(model_name)
-    audios = get_audio(args.pop("video"))
-    subtitles = get_subtitles(
-        audios, output_srt or srt_only, output_dir, lambda audio_path: model.transcribe(audio_path, **args)
+    audios = get_audio(good_video_pathes)
+    subtitles_path = get_subtitles(
+        audios, output_srt or srt_only, output_dir, lambda audio_path: model.transcribe(str(audio_path), **args)
     )
 
     if srt_only:
         return
 
-    for path, srt_path in subtitles.items():
+    for path, srt_path in subtitles_path.items():
+        srt_path = Path(srt_path).resolve()
         out_path = os.path.join(output_dir, f"{filename(path)}.mp4")
 
-        print(f"Adding subtitles to {filename(path)}...")
+        if not (os.path.exists(srt_path) and os.path.exists(path)):
+            print("Missing srt or source file, something went wrong")
+            continue
 
-        video = ffmpeg.input(path)
-        audio = video.audio
+        command = build_ffmpeg_command(path,srt_path,out_path)
+        execute_ffmpeg_command(command)
 
-        ffmpeg.concat(
-            video.filter('subtitles', srt_path, force_style="OutlineColour=&H40000000,BorderStyle=3"), audio, v=1, a=1
-        ).output(out_path).run(quiet=True, overwrite_output=True)
+def to_raw_path(path:str) -> str:
+    return path.replace('\\','/')
 
-        print(f"Saved subtitled video to {os.path.abspath(out_path)}.")
-
-
-def get_audio(paths):
+def get_audio(source_file_path_arr: List[Path]) -> Dict[Path, Path]:
     temp_dir = tempfile.gettempdir()
 
     audio_paths = {}
 
-    for path in paths:
-        print(f"Extracting audio from {filename(path)}...")
-        output_path = os.path.join(temp_dir, f"{filename(path)}.wav")
+    for source_file in source_file_path_arr:
+        print(f"Extracting audio from {os.path.basename(source_file)}...")
+        output_path = Path(temp_dir) / f"{Path(source_file).stem}.wav"
 
-        ffmpeg.input(path).output(
-            output_path,
+        ffmpeg.input(filename=str(source_file)).output(
+            str(output_path),
             acodec="pcm_s16le", ac=1, ar="16k"
         ).run(quiet=True, overwrite_output=True)
 
-        audio_paths[path] = output_path
+        audio_paths[source_file] = output_path
+            
 
     return audio_paths
 
 
-def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcribe: callable):
+def get_subtitles(
+    audio_paths: Dict[Path, Path],
+    output_srt: bool,
+    output_dir: str,
+    transcribe: callable,
+) -> Dict[Path, Path]:
     subtitles_path = {}
 
     for path, audio_path in audio_paths.items():
-        srt_path = output_dir if output_srt else tempfile.gettempdir()
-        srt_path = os.path.join(srt_path, f"{filename(path)}.srt")
-        
-        print(
-            f"Generating subtitles for {filename(path)}... This might take a while."
-        )
+        srt_dir = Path(output_dir) if output_srt else Path(tempfile.gettempdir())
+        srt_path = srt_dir / f"{Path(path).stem}.srt"
+
+        # Skip if file already exists
+        if srt_path.exists():
+            print(f"Subtitle file for {Path(path).name} already exists. Skipping...")
+            subtitles_path[path] = str(srt_path)
+            continue
+
+        print(f"Generating subtitles for {Path(path).name}... This might take a while.")
 
         warnings.filterwarnings("ignore")
         result = transcribe(audio_path)
@@ -106,10 +121,34 @@ def get_subtitles(audio_paths: list, output_srt: bool, output_dir: str, transcri
         with open(srt_path, "w", encoding="utf-8") as srt:
             write_srt(result["segments"], file=srt)
 
-        subtitles_path[path] = srt_path
+        subtitles_path[path] = str(srt_path)
 
     return subtitles_path
 
+def execute_ffmpeg_command(command):
+    try:
+        # Run the command and wait for it to complete
+        subprocess.run(command, check=True, shell=True)
+        print("Command executed successfully.")
+    except subprocess.CalledProcessError as e:
+        # Handle errors in the subprocess
+        print(f"An error occurred: {e}")
+
+def build_ffmpeg_command(video_path, subtitle_path, output_path):
+    video_path = str(video_path)
+    subtitle_path = str(subtitle_path)
+    output_path = str(output_path)
+    # Replace backslashes with forward slashes
+    subtitle_path = subtitle_path.replace("\\", "\\\\")
+    if ':' in subtitle_path:
+        subtitle_path =  subtitle_path.replace(":", "\\:")
+
+    command = (
+        f'ffmpeg -i "{video_path}" -filter_complex '
+        f'"[0:v]subtitles=\'{subtitle_path}\'[v]" -map "[v]" -map "0:a" -y "{output_path}"'
+    )
+    
+    return command
 
 if __name__ == '__main__':
     main()
